6630359f27c5dd030bdd799d523f8e435c2e583e
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2026-09-18 12:38:00 +0200

    COmpleted database connection using Docker and run migrations and Update-database

---
+++ abcProperties/abcProperties/Infrastructure/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    //We register our dependencies in this class, so we can use them in our application
+    public static class StartUp
+    {
+        //We gonna configuration to get our connection string from appsettings.json file, so we need to add it as a parameter in this method
+        //THis IServiceCollection is a not a parameter is an extension method, so we can use it in the Program.cs file to register our dependencies
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services,IConfiguration configuration) {
+
+            // Get the connection string from appsettings.json
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            // Make sure the connection string exists
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException(
+                    "Connection string 'DefaultConnection' not found in configuration.");
+            }
+
+            //Register DbContext 
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                //We will use SQL Server as our database provider
+                options.UseSqlServer(connectionString, sqlOptions =>
+                {   /// Rename the EF core tabble to "Migrations" and place it in the "EFCore" schema 
+                    // Store EF Core migrations history in a custom table/schema
+                    sqlOptions.MigrationsHistoryTable("Migrations", "EFCore");
+
+                    // Retry the SQL Server operation if a temporary connection
+                    // failure occurs
+                    sqlOptions.EnableRetryOnFailure();
+                } 
+                
+               );
+
+            }
+            
+            );
+
+
+            // Register other infrastructure services here
+            return services;
+        }
+
+    }
+}
--- abcProperties/abcProperties/Presentation/Program.cs
+++ abcProperties/abcProperties/Presentation/Program.cs
@@ -1,3 +1,5 @@
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+/*Add Infrastructure Services*/
+//We must pass the configuration parameter to the AddInfrastructureService method, so we can get our connection string from appsettings.json file
+//configurations are found within the builder .Configuration property, which is an instance of IConfiguration. This property is automatically populated with the configuration settings from various sources, including appsettings.json, environment variables, and command-line arguments. By passing builder.Configuration to the AddInfrastructureService method, we ensure that our infrastructure services have access to the necessary configuration settings, such as the database connection string.
+builder.Services.AddInfrastructureService(builder.Configuration);
 
 var app = builder.Build();
 
