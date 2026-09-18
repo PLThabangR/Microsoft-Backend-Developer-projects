c27682a41e3bd3ecea3081048a0a72d411b0a9fb
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2026-09-18 11:19:18 +0200

    Created entities and completed the relationship between agent and property

---
+++ abcProperties/abcProperties/Domain/Entities/Agent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Agent
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        //Agent can have a list of properties/listings
+        //Agent can have many properties, so we will have a navigation property to the Property class
+        public List<Property> PropertyListings { get; set; }
+    }
+}
---
+++ abcProperties/abcProperties/Domain/Entities/Property.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Property
+    {
+        public int Id { get; set; }
+
+        //this is our foreign key to the Agent table
+        public int AgentId { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime ListingDate { get; set; }
+
+        //A propperty belog to a specific agent, so we will have a navigation property to the Agent class
+        public Agent Agent { get; set; }
+    }
+}
